e44e0d2a392fada64165d91bcdb7f2c4646d2470
Parampreet Singh <pampi@192.168.1.11>
2026-03-02 19:01:09 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  
+  res.json({ message: 'Hello, World!' });
+});
+app.get('/cloud', (req, res) => {
+  res.json({ message: 'Welcome to the CI/CD Project' });
+});
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
