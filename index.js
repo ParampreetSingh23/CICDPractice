10d31c11f25e01de762f557acd6221a7bbf19d3b
Parampreet Singh <pampi@192.168.1.24>
2026-03-02 20:46:35 +0530

    Updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 
 app.get('/', (req, res) => {
   
-  res.json({ message: 'Hello, World!' });
+  res.json({ message: 'Hello to my first CI/CD Pipeline' });
 });
 app.get('/cloud', (req, res) => {
   res.json({ message: 'Welcome to the CI/CD Project BASICS github actions' });
